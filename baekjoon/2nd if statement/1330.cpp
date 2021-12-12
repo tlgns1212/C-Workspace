f2af850bfa7231ae6a15a4334f9c8f4e8c80a0f7
Sihoon <tlgns1212@naver.com>
2021-12-12 21:49:47 +0900

    problem 1330 solved

---
+++ baekjoon/2nd if statement/1330.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    /*     if (A < B)
+        printf("<");
+    else if (A > B)
+        printf(">");
+    else
+        printf("=="); */
+    printf("%s", A < B ? "<" : A > B ? ">"
+                                     : "==");
+}
