84c5d277e3742807772a6efd07f29e07842bbbbe
Sihoon <tlgns1212@naver.com>
2021-12-13 18:05:06 +0900

    problem 2753 with and/or

--- baekjoon/2nd if statement/2753.cpp
+++ baekjoon/2nd if statement/2753.cpp
@@ -3,5 +3,6 @@ int main()
 {
     int year;
     scanf("%d", &year);
-    printf("%d", year % 4 == 0 ? (year % 400 == 0 ? 1 : (year % 100 != 0 ? 1 : 0)) : 0);
+    // printf("%d", year % 4 == 0 ? (year % 400 == 0 ? 1 : (year % 100 != 0 ? 1 : 0)) : 0);
+    printf("%d", (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
 }
