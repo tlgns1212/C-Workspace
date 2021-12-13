6c2c14a0af2381ee8ab0e3e7be4f5b9c64781d9b
Sihoon <tlgns1212@naver.com>
2021-12-14 08:29:28 +0900

    problem 2884 refered

--- baekjoon/2nd if statement/2884.cpp
+++ baekjoon/2nd if statement/2884.cpp
@@ -3,18 +3,19 @@ int main()
 {
     int hour, minute;
     scanf("%d %d", &hour, &minute);
-    if (minute < 45)
-    {
-        if (hour < 1)
-        {
-            hour = 24;
-        }
-        hour -= 1;
-        minute += 15;
-    }
-    else
-    {
-        minute -= 45;
-    }
-    printf("%d %d", hour, minute);
+    // if (minute < 45)
+    // {
+    //     if (hour < 1)
+    //     {
+    //         hour = 24;
+    //     }
+    //     hour -= 1;
+    //     minute += 15;
+    // }
+    // else
+    // {
+    //     minute -= 45;
+    // }
+    // printf("%d %d", hour, minute);
+    printf("%d %d", (hour - (minute < 45) + 24) % 24, (minute - 45 + 60) % 60);
 }
