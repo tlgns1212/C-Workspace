37766db7d35d7b4193c4f9e6b1b2f76165a34934
Sihoon <tlgns1212@naver.com>
2021-12-17 18:12:42 +0900

    10871 solved

---
+++ baekjoon/3rd for statement/10871.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int N, X, A;
+    scanf("%d %d", &N, &X);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A);
+        if (A < X)
+        {
+            printf("%d ", A);
+        }
+    }
+}
