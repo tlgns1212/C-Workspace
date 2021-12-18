66238e0f026fa72ef26cfaacf6e62c8a06a3000d
Sihoon <tlgns1212@naver.com>
2021-12-18 23:31:13 +0900

    10818 solved c++ version

--- baekjoon/5th 1st dimensional array/10818.cpp
+++ baekjoon/5th 1st dimensional array/10818.cpp
@@ -1,15 +1,38 @@
+// #include <iostream>
+// int main()
+// {
+//     int A, N, min = 1000001, max = -1000001;
+//     scanf("%d", &N);
+//     for (int i = 0; i < N; i++)
+//     {
+//         scanf("%d", &A);
+//         if (min > A)
+//             min = A;
+//         if (max < A)
+//             max = A;
+//     }
+//     printf("%d %d", min, max);
+// }
+
 #include <iostream>
+#include <vector>
+using namespace std;
+
 int main()
 {
-    int A, N, min = 1000001, max = -1000001;
-    scanf("%d", &N);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, temp, min = 1000000, max = -1000000;
+    cin >> N;
     for (int i = 0; i < N; i++)
     {
-        scanf("%d", &A);
-        if (min > A)
-            min = A;
-        if (max < A)
-            max = A;
+        cin >> temp;
+        if (temp < min)
+            min = temp;
+        if (max < temp)
+            max = temp;
     }
-    printf("%d %d", min, max);
+    cout << min << ' ' << max;
 }
