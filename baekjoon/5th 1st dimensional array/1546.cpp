706d673ec98c2f586a8d19b5031a60bfea56afe2
Sihoon <tlgns1212@naver.com>
2021-12-19 13:49:36 +0900

    1546 easier coding

--- baekjoon/5th 1st dimensional array/1546.cpp
+++ baekjoon/5th 1st dimensional array/1546.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int N, max = 0, sum[1000] = {};
+//     double aver = 0;
+//     cin >> N;
+//     for (int i = 0; i < N; i++)
+//     {
+//         cin >> sum[i];
+//         if (max < sum[i])
+//             max = sum[i];
+//     }
+//     for (int i = 0; i < N; i++)
+//     {
+//         aver += sum[i] / (double)max * 100;
+//     }
+//     cout << aver / N;
+// }
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, max = 0, sum[1000] = {};
-    double aver = 0;
+    int N, max = 0, num, total = 0;
     cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> sum[i];
-        if (max < sum[i])
-            max = sum[i];
+    for (int i = 0;i<N;i++){
+        cin >> num;
+        total += num;
+        if (num > max)
+            max = num;
     }
-    for (int i = 0; i < N; i++)
-    {
-        aver += sum[i] / (double)max * 100;
-    }
-    cout << aver / N;
+    cout << (double)total/(N*max)*100;
 }
