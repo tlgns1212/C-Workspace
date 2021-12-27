a7723e3ba07077748e60d4c72bdfc13b346ccdb9
Sihoon <tlgns1212@naver.com>
2021-12-27 13:41:22 +0900

    1065 solved easier

--- baekjoon/6th function/1065.cpp
+++ baekjoon/6th function/1065.cpp
@@ -1,39 +1,56 @@
 #include <iostream>
 using namespace std;
 
-bool isHansoo(int N)
-{
-    int changed, a;
-    if (N < 100)
-        return true;
-    else
-    {
-        changed = N % 10 - (N / 10) % 10;
-        a = N % 10;
-        N /= 10;
-        while (N)
-        {
-            if (changed != (a - N % 10))
-                return false;
-            a = N % 10;
-            N /= 10;
-        }
-    }
-    return true;
-}
+// bool isHansoo(int N)
+// {
+//     int changed, a;
+//     if (N < 100)
+//         return true;
+//     else
+//     {
+//         changed = N % 10 - (N / 10) % 10;
+//         a = N % 10;
+//         N /= 10;
+//         while (N)
+//         {
+//             if (changed != (a - N % 10))
+//                 return false;
+//             a = N % 10;
+//             N /= 10;
+//         }
+//     }
+//     return true;
+// }
 
-int main()
-{
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int N, count = 0;
+//     cin >> N;
+//     for (int i = 1; i <= N; i++)
+//     {
+//         if (isHansoo(i) == true)
+//             count++;
+//     }
+//     cout << count;
+// }
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, count = 0;
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-    {
-        if (isHansoo(i) == true)
-            count++;
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 1;i<=n;i++){
+        if (i < 100)
+            cnt ++;
+        else
+            if ((i/100-(i/10)%10) == ((i/10)%10-i%10))
+                cnt++;
     }
-    cout << count;
+    cout << cnt;
 }
