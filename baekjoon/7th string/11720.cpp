560b88d3a20b206ed466c8abacd2b35641859f47
Sihoon <tlgns1212@naver.com>
2021-12-28 19:59:31 +0900

    11720 not solved yet

---
+++ baekjoon/7th string/11720.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    char a[100];
+    int N, sum = 0;
+
+    cin >> N;
+    cin >> a;
+    for (int i = 0;i<N;i++){
+        sum += (int)a[i] - 48;
+    }
+    cout << sum;
+}
