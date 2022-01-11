7fd88f8895bc1c9cd6b278d6d4095e1ff67d4a0a
Sihoon <tlgns1212@naver.com>
2022-01-11 19:30:11 +0900

    self study 1-1

--- 열혈C++프로그래밍/problem1-1.cpp
+++ 열혈C++프로그래밍/problem1-1.cpp
@@ -7,9 +7,9 @@ int main()
     int sum = 0;
     for (int i = 1; i < +5; i++)
     {
-        cout << i << '번째 정수 입력: ' << i;
+        cout << i << "번째 정수 입력: " << i << '\n';
         sum += i;
     }
-    cout << '합계: ' << sum;
+    cout << "합계: " << sum;
 }
 // 터미널에서 한글 나오게 바꾸는거 내용이랑 방법 추가하기
