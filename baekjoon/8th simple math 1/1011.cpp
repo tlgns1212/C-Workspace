4f1f9ab4cb31efa49377178042ba678022f8b83b
Sihoon <tlgns1212@naver.com>
2022-01-12 21:44:41 +0900

    1011 doing

---
+++ baekjoon/8th simple math 1/1011.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T, x, y;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int count = 0;
+        cin >> x >> y;
+        while (x >= y)
+        {
+        }
+    }
+}
+// 하는중
