7cd4c06952d5b5999537dc82afdf422e92d739ea
Sihoon <tlgns1212@naver.com>
2022-01-13 23:08:01 +0900

    1011 first try divided by half

--- baekjoon/8th simple math 1/1011.cpp
+++ baekjoon/8th simple math 1/1011.cpp
@@ -3,15 +3,35 @@ using namespace std;
 
 int main()
 {
-    int T, x, y;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T, x, y, count = 0, i, j;
+    double half;
     cin >> T;
-    for (int i = 0; i < T; i++)
+    for (i = 0; i < T; i++)
     {
-        int count = 0;
         cin >> x >> y;
-        while (x >= y)
+        half = ((double)y - x) / 2;
+        for (j = 1;; j++)
         {
+            count += j;
+            if (count > half)
+            {
+                count -= j;
+                break;
+            }
         }
+        count = y - x - (2 * count);
+        if (count <= j)
+        {
+            j = (j - 1) * 2 + 1;
+        }
+        else if (count > j)
+        {
+            j = (j - 1) * 2 + 2;
+        }
+        cout << j << '\n';
     }
 }
-// 하는중
