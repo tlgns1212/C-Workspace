7ffee94ccbc7df3953c20a3834626f23be5dbf07
Sihoon <tlgns1212@naver.com>
2022-01-14 23:15:55 +0900

    1978 solved easier with no recursive function

--- baekjoon/9th simple math 2/1978.cpp
+++ baekjoon/9th simple math 2/1978.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 using namespace std;
 
-bool isPrimeNum(int n)
-{
-    if (n <= 1)
-    {
-        return false;
-    }
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            if (i * j == n)
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
+// bool isPrimeNum(int n)
+// {
+//     if (n <= 1)
+//     {
+//         return false;
+//     }
+//     for (int i = 1; i < n; i++)
+//     {
+//         for (int j = i; j < n; j++)
+//         {
+//             if (i * j == n)
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int N, count = 0;
+//     cin >> N;
+//     while (N--)
+//     {
+//         int n;
+//         cin >> n;
+//         if (isPrimeNum(n))
+//         {
+//             count++;
+//         }
+//     }
+//     cout << count;
+// }
 
 int main()
 {
@@ -26,16 +46,22 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, count = 0;
-    cin >> N;
-    while (N--)
+    int n, count = 0, num;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        int n;
-        cin >> n;
-        if (isPrimeNum(n))
-        {
+        cin >> num;
+        if (num == 1)
             count++;
-        }
+        else
+            for (int j = 2; j < num; j++)
+            {
+                if (num % j == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
     }
-    cout << count;
+    cout << n - count;
 }
