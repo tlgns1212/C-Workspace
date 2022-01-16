48fb0f19d0e2846a0349ffe7f3e58b0d4a72058a
Sihoon <tlgns1212@naver.com>
2022-01-16 21:59:13 +0900

    2581 solved with other peoples code(just by shrinking the loop in the for statement, 16ms -> 0ms)

--- baekjoon/9th simple math 2/2581.cpp
+++ baekjoon/9th simple math 2/2581.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int sum = 0, M, N, minNum;
+//     cin >> M >> N;
+//     for (int i = M; i <= N; i++)
+//     {
+//         if (i == 2)
+//         {
+//             if (sum == 0)
+//             {
+//                 minNum = i;
+//             }
+//             sum += i;
+//         }
+//         for (int j = 2; j < i; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 break;
+//             }
+//             else if (j == (i - 1))
+//             {
+//                 if (sum == 0)
+//                 {
+//                     minNum = i;
+//                 }
+//                 sum += i;
+//             }
+//         }
+//     }
+//     if (sum != 0)
+//     {
+//         cout << sum << '\n'
+//              << minNum;
+//     }
+//     else
+//     {
+//         cout << -1;
+//     }
+// }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -9,30 +54,27 @@ int main()
 
     int sum = 0, M, N, minNum;
     cin >> M >> N;
+    if (M == 1)
+    {
+        M = 2;
+    }
     for (int i = M; i <= N; i++)
     {
-        if (i == 2)
+        int temp = 0;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                temp = 1;
+            }
+        }
+        if (temp == 0)
         {
             if (sum == 0)
             {
                 minNum = i;
             }
             sum += i;
-        }
-        for (int j = 2; j < i; j++)
-        {
-            if (i % j == 0)
-            {
-                break;
-            }
-            else if (j == (i - 1))
-            {
-                if (sum == 0)
-                {
-                    minNum = i;
-                }
-                sum += i;
-            }
         }
     }
     if (sum != 0)
