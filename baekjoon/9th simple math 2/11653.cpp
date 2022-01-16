6ed3e625eb6ef671b075db12502575474a3b89cb
Sihoon <tlgns1212@naver.com>
2022-01-16 22:58:54 +0900

    11653 solved with other codes(16ms -> 0ms, just by correcting how loops were designed)

--- baekjoon/9th simple math 2/11653.cpp
+++ baekjoon/9th simple math 2/11653.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int N, i = 1;
+//     cin >> N;
+//     while (N != 1)
+//     {
+//         i++;
+//         if (N % i == 0)
+//         {
+//             N /= i;
+//             cout << i << '\n';
+//             i = 1;
+//         }
+//         if (i == N)
+//         {
+//             break;
+//         }
+//     }
+// }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, i = 1;
+    int N;
     cin >> N;
-    while (N != 1)
+    for (int i = 2; i * i <= N; i++)
     {
-        i++;
-        if (N % i == 0)
+        while (N % i == 0)
         {
-            N /= i;
             cout << i << '\n';
-            i = 1;
+            N /= i;
         }
-        if (i == N)
-        {
-            break;
-        }
+    }
+    if (N > 1)
+    {
+        cout << N;
     }
 }
