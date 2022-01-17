a4f3e6e1025843579dc5f94d7a2e6046b0de5026
Sihoon <tlgns1212@naver.com>
2022-01-17 22:55:33 +0900

    1929 solved (1912ms -> 12ms) with others code, used boolean and efficient use of loops

--- baekjoon/9th simple math 2/1929.cpp
+++ baekjoon/9th simple math 2/1929.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int M, N;
+//     cin >> M >> N;
+//     if (M == 1)
+//     {
+//         M = 2;
+//     }
+//     for (int i = M; i <= N; i++)
+//     {
+//         int temp = 0;
+//         for (int j = 2; j * j <= i; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 temp = 1;
+//             }
+//         }
+//         if (temp == 0)
+//         {
+//             cout << i << '\n';
+//         }
+//     }
+// }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -8,22 +37,27 @@ int main()
     cout.tie(NULL);
 
     int M, N;
+    bool arr[1000001];
     cin >> M >> N;
-    if (M == 1)
+    for (int i = 2; i < 1000001; i++)
     {
-        M = 2;
+        arr[i] = true;
+    }
+    arr[0] = arr[1] == false;
+
+    for (int i = 2; (i * i) <= N; i++)
+    {
+        if (arr[i])
+        {
+            for (int j = i * i; j <= N; j += i)
+            {
+                arr[j] = false;
+            }
+        }
     }
     for (int i = M; i <= N; i++)
     {
-        int temp = 0;
-        for (int j = 2; j * j <= i; j++)
-        {
-            if (i % j == 0)
-            {
-                temp = 1;
-            }
-        }
-        if (temp == 0)
+        if (arr[i])
         {
             cout << i << '\n';
         }
