16d3565c787d1a2a0737ec490e85b7c22e13d24e
Sihoon <tlgns1212@naver.com>
2022-01-25 21:45:22 +0900

    4153 solved (0ms -> 0ms) (no need to think hard, if one point is 0, others are too, and no need to use ?: if you can finish it with simple if quote.)

--- baekjoon/9th simple math 2/4153.cpp
+++ baekjoon/9th simple math 2/4153.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int a, b, c;
+//     while (1)
+//     {
+//         cin >> a >> b >> c;
+//         if ((a == 0 && b == 0) && c == 0)
+//         {
+//             break;
+//         }
+//         a *= a;
+//         b *= b;
+//         c *= c;
+//         cout << ((a + b == c) ? "right" : ((a + c == b) ? "right" : ((b + c == a) ? "right" : "wrong"))) << endl;
+//     }
+// }
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -11,13 +31,15 @@ int main()
     while (1)
     {
         cin >> a >> b >> c;
-        if ((a == 0 && b == 0) && c == 0)
-        {
+        if (a == 0)
             break;
+        if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+        {
+            cout << "right" << endl;
         }
-        a *= a;
-        b *= b;
-        c *= c;
-        cout << ((a + b == c) ? "right" : ((a + c == b) ? "right" : ((b + c == a) ? "right" : "wrong"))) << endl;
+        else
+        {
+            cout << "wrong" << endl;
+        }
     }
 }
