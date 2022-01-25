7c9099397e4a72032f83ec6691aa1b6134e1e9e0
Sihoon <tlgns1212@naver.com>
2022-01-25 21:39:42 +0900

    4153 solved (0ms)

---
+++ baekjoon/9th simple math 2/4153.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c;
+    while (1)
+    {
+        cin >> a >> b >> c;
+        if ((a == 0 && b == 0) && c == 0)
+        {
+            break;
+        }
+        a *= a;
+        b *= b;
+        c *= c;
+        cout << ((a + b == c) ? "right" : ((a + c == b) ? "right" : ((b + c == a) ? "right" : "wrong"))) << endl;
+    }
+}
