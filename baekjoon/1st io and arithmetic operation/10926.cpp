4502c99ff19bbf6f94d8f6742f6e57ba85cde759
Sihoon <tlgns1212@naver.com>
2022-01-27 13:11:24 +0900

    10926 solved (0ms)

---
+++ baekjoon/1st io and arithmetic operation/10926.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    char name[55];
+    cin >> name;
+    cout << name << "??!";
+}
