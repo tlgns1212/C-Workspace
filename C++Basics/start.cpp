d3a02b22e97ca21b7287537d95638d1a9cd10206
Sihoon <tlgns1212@naver.com>
2022-01-27 20:56:55 +0900

    can debug now (changed the mingw settings)

--- C++Basics/start.cpp
+++ C++Basics/start.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     // std::cout<<"Hello World!"<<std::endl;
     // std::cout<<"Hello World!"<<std::endl;
     // getchar();
     // return 0;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
     cout << "Hello World" << endl;
     getchar();
     return 0;
