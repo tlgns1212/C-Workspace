2daeb7b3386e1fe037e0bcaab929d6bf80076c4a
Sihoon <tlgns1212@naver.com>
2022-01-28 11:33:24 +0900

    10870 solved (0ms)

---
+++ baekjoon/10th recursive function/10870.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int Fibonachi(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << Fibonachi(n);
+}
