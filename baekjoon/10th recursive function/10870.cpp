36c94b7d5a60bca8f5f2c929745f678f4ba95a0a
Sihoon <tlgns1212@naver.com>
2022-01-28 11:36:01 +0900

    10870 solved (0ms -> 0ms) (think it is better using loops instead of recursive functions)

--- baekjoon/10th recursive function/10870.cpp
+++ baekjoon/10th recursive function/10870.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int Fibonachi(int n)
-{
-    if (n == 0)
-    {
-        return 0;
-    }
-    else if (n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return Fibonachi(n - 1) + Fibonachi(n - 2);
-    }
-}
+// int Fibonachi(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     else if (n == 1)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return Fibonachi(n - 1) + Fibonachi(n - 2);
+//     }
+// }
 
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     int n;
+//     cin >> n;
+//     cout << Fibonachi(n);
+// }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
+    int n, arr[30] = {0, 1};
     cin >> n;
-    cout << Fibonachi(n);
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    cout << arr[n];
 }
