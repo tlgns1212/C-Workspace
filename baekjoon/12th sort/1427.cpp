4a3049c29aed023b5ff2701fb20ec5acbe8c397f
Sihoon <tlgns1212@naver.com>
2022-02-08 13:22:50 +0900

    1427 solved (0ms)

---
+++ baekjoon/12th sort/1427.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    sort(str.begin(), str.end(), greater<char>());
+    cout << str;
+}
