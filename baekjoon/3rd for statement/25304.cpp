c8cdbaa08826b81a9b8182e1190602bc5f2ec595
Sihoon Kim <tlgns1212@naver.com>
2022-10-10 12:18:39 +0900

    (22/10/10) new for statements solved

---
+++ baekjoon/3rd for statement/25304.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X, N, a, b;
+    cin >> X;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a >> b;
+        X -= (a * b);
+    }
+    if (X == 0)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
