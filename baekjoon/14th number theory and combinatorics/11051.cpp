7f70d60e00f835eef3805ad1d278c5c039c680fe
Sihoon Kim <tlgns1212@naver.com>
2022-10-19 22:31:52 +0900

    (22/10/19) baekjoon solved

---
+++ baekjoon/14th number theory and combinatorics/11051.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K, answer = 1;
+    cin >> N >> K;
+
+    for (int i = 0; i < K; i++)
+    {
+        answer *= N - i;
+        answer /= i + 1;
+        answer %= 10007;
+        if (answer == 0)
+            answer = 1;
+    }
+
+    cout << answer;
+    return 0;
+}
