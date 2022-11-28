4636b818681c4934cf87242dde697058ced93c85
Sihoon Kim <tlgns1212@naver.com>
2022-11-28 15:47:29 +0900

    (22/11/28) baekjoon solved

---
+++ baekjoon/problems/20254.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << 56 * A + 24 * B + 14 * C + 6 * D;
+
+    return 0;
+}
