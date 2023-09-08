93103096bf6da25706f4aad3d2435ff0dd01a00c
Sihoon Kim <tlgns1212@naver.com>
2023-09-08 09:17:08 +0900

    10101

---
+++ baekjoon/9th simple math 2/10101.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	if (a + b + c == 180) {
+		if (a == b && b == c)
+			cout << "Equilateral";
+		else if (a == b || b == c || a == c)
+			cout << "Isosceles";
+		else
+			cout << "Scalene";
+	}
+	else
+		cout << "Error";
+	return 0;
+}
