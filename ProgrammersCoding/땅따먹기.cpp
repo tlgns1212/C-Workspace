39c524c29d94d1757bd918a7c9d5efa87c3725b4
Sihoon Kim <53401308+tlgns1212@users.noreply.github.com>
2024-04-18 19:23:56 +0900

    programmers 땅따먹기 solved

---
+++ ProgrammersCoding/땅따먹기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int>> land)
+{
+    int answer = 0, N = land.size();
+    vector<vector<int>> dp(N, vector<int>(4,0));
+    
+    for(int i = 0; i < 4; i++){
+        dp[0][i] = land[0][i];
+    }
+    
+    for(int i = 1; i < N; i++){
+        for(int j = 0; j < 4; j++){
+            for(int k = 0; k < 4; k++){
+                if(j == k) continue;
+                dp[i][j] = max(dp[i][j], dp[i-1][k] + land[i][j]);
+            }
+        }
+    }
+    
+    for(int i = 0; i < 4; i++){
+        answer = max(answer, dp[N-1][i]);
+    }
+    
+    
+
+    return answer;
+}
