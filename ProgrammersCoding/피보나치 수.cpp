c3a3c3221fc8a346dfae02b25a704b892638cb5d
Sihoon Kim <53401308+tlgns1212@users.noreply.github.com>
2024-04-22 11:52:23 +0900

    programmers 피보나치 수 solved

---
+++ ProgrammersCoding/피보나치 수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> dp(100001,0);
+    dp[1] = 1;
+    for(int i = 2; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    answer = dp[n];
+    return answer;
+}
